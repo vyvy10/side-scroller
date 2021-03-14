286e9b39abb3a13b9d60a7e53e8b5ffdfbe5e6e1
vyvy10 <79157046+vyvy10@users.noreply.github.com>
2021-03-14 18:37:46 -0400

    Create obstacle.js
    Obstacle 1

---
+++ obstacle.js
@@ -0,0 +1,33 @@
+class Obstacle {
+  constructor(img,x) {
+    this.pos = createVector(400+x*100, height - 50-x*70);
+    this.vel = createVector(0, 0*x);
+    this.acc = createVector(0, 0);
+    this.pic = img;
+  }
+  
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+  }
+  
+   applyForce(force) {
+    this.acc.add(force);
+  }
+  
+   display() {
+    fill(25, 150,30);
+    stroke(255);
+    //rect(this.pos.x, this.pos.y, 50, 40);
+    image(this.pic,this.pos.x, this.pos.y, 50, 40);
+  }
+  
+  edges() {
+    if (this.pos.y > height-50) {
+      this.vel.y = 0;
+      this.pos.y = height-50;
+    }
+  }
+  
+}
